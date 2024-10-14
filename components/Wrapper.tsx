34e9677739e92195073e7e589bc9421ad8ac81d9
rohitcoder1976 <rmarath2008@gmail.com>
2024-10-15 02:50:17 +0530

    Added wrapper

---
+++ components/Wrapper.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Button } from './ui/button';
+import { Checkbox } from './ui/checkbox';
+import { SearchInput } from './ui/searchInput';
+
+const Wrapper = () => {
+  return (
+    <div>
+      <Button></Button>
+      <Checkbox></Checkbox>
+      <SearchInput></SearchInput>
+    </div>
+  )
+}
+
+export default Wrapper;
